1b38e18a17dccd9b69d35e30e3566a8717dfdf43
Akio Tamashiro <angelakio.tamashiro@gmail.com>
2021-09-20 00:39:54 -0300

    Funciones para obtener y setear datos mediante SP

--- control_stock_app/views.py
+++ control_stock_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.db import connection
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
 from .serializers import *
+from .data_access import db_helper as _db
 
 
 @api_view(['POST'])
@@ -22,8 +24,10 @@ def login(request):
         except Exception as ex:
             return Response('Error al intentar autentificar', status=status.HTTP_400_BAD_REQUEST)
 
+        _user_info = _db.get_data_from_procedure(connection=connection,
+                                                 proc_name='sp_get_user_info')
         _response = UserSerializer(_objUser, many=False)
 
-        return Response(_response.data, status=status.HTTP_200_OK)
+        return Response(_user_info, status=status.HTTP_200_OK)
     except:
         return Response('Server Error',status=status.HTTP_500_INTERNAL_SERVER_ERROR)
